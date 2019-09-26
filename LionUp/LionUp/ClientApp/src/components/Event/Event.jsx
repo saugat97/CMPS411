87ea6ba18b995943c09b82810e1fd5b2f13f8257
@sijantiwari12 <sijan.tiwari@selu.edu>
2019-09-25 20:30:17 -0500

    embeded twiter API

--- LionUp/LionUp/ClientApp/src/components/Event/Event.jsx
+++ LionUp/LionUp/ClientApp/src/components/Event/Event.jsx
@@ -1,8 +1,16 @@
+/// <reference path="../home/home.jsx" />
 import React, { Component } from "react";
+import Home from "../Home/Home.jsx";
+
 
 class Event extends Component {
   render() {
-    return <div>This is Event</div>;
-  }
+      return (
+          <view>
+          <div>This is Event</div>
+              <div><Home /></div>
+              </view>
+              )
+    };
 }
 export default Event;
--- LionUp/LionUp/ClientApp/src/components/Home/Home.jsx
+++ LionUp/LionUp/ClientApp/src/components/Home/Home.jsx
@@ -1,68 +1,69 @@
 import React, { Component, Fragment } from "react";
 import Nav from "./Nav";
-import { Grid, Button, Container, Segment } from "semantic-ui-react";
+import { Grid, Button, Container, Segment, Card } from "semantic-ui-react";
+import { Tweet } from 'react-twitter-widgets'
+import './Home.css';
 
 class Home extends Component {
-  render() {
-    return (
-      <Fragment>
-        <Nav />
-        <Container className="main">
-          <Grid>
-            <Grid.Column width={13}>
-              <div className="segment">
-                <div className="label-quick-link">Home</div>
-                <img
-                  src="/assets/img1.jpg"
-                  className="feed-image"
-                  alt="image1"
-                />
-                <h2>Career Fair 2019</h2>
-                <div className="event-info">
-                  <p>Thursday, September 19, 2019</p>
-                  <p> 9:00am - 2:30pm</p>
-                  <p> Pennington Student Activity Center</p>
-                </div>
-              </div>
-            </Grid.Column>
+    
+    
 
-            <Grid.Column width={3}>
-              <div className="segment">
-                <div className="label-quick-link">Quick Links</div>
-                <a href="https://www.southeastern.edu/leonet/">
-                  <img
-                    src="/assets/leonet.png"
-                    className="quicklink-image"
-                    alt="Selu Leonet"
-                  />
-                </a>
-                <a href="https://moodle.selu.edu/moodle/">
-                  <img
-                    src="/assets/moodle.png"
-                    className="quicklink-image"
-                    alt="Selu Moodle"
-                  />
-                </a>
-                <a href="https://www2.southeastern.edu/external/google_login/">
-                  <img
-                    src="/assets/webmail.png"
-                    className="quicklink-image"
-                    alt="Selu Webmail"
-                  />
-                </a>
-                <a href="https://psrv8.selu.edu/psp/hrprd/EMPLOYEE/HRMS/h/?tab=DEFAULT&cmd=login&languageCd=ENG&">
-                  <img
-                    src="/assets/timeandlabor.png"
-                    className="quicklink-image"
-                    alt="Selu Time"
-                  />
-                </a>
-              </div>
-            </Grid.Column>
-          </Grid>
-        </Container>
-      </Fragment>
-    );
-  }
+    render() {
+        return (
+            <Fragment>
+                <Nav />
+                <Container className="main">
+                    <Grid>
+                        <Grid.Column width={3}>
+                            <div className="segment" style={{ height: '500px'}}>
+
+                                </div>
+                        </Grid.Column>
+                        <Grid.Column width={10}>
+                            <Card style={{ width:'600px'}}>
+                            <div>
+                                <a class="twitter-timeline" data-width="600px" href="https://twitter.com/oursoutheastern?ref_src=twsrc%5Etfw">Know more about Southeastern</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                                </div>
+                                </Card>
+                        </Grid.Column>
+
+                        <Grid.Column width={3}>
+                            <div className="segment">
+                                <div className="label-quick-link">Quick Links</div>
+                                <a href="https://www.southeastern.edu/leonet/">
+                                    <img
+                                        src="/assets/leonet.png"
+                                        className="quicklink-image"
+                                        alt="Selu Leonet"
+                                    />
+                                </a>
+                                <a href="https://moodle.selu.edu/moodle/">
+                                    <img
+                                        src="/assets/moodle.png"
+                                        className="quicklink-image"
+                                        alt="Selu Moodle"
+                                    />
+                                </a>
+                                <a href="https://www2.southeastern.edu/external/google_login/">
+                                    <img
+                                        src="/assets/webmail.png"
+                                        className="quicklink-image"
+                                        alt="Selu Webmail"
+                                    />
+                                </a>
+                                <a href="https://psrv8.selu.edu/psp/hrprd/EMPLOYEE/HRMS/h/?tab=DEFAULT&cmd=login&languageCd=ENG&">
+                                    <img
+                                        src="/assets/timeandlabor.png"
+                                        className="quicklink-image"
+                                        alt="Selu Time"
+                                    />
+                                </a>
+                            </div>
+                        </Grid.Column>
+                    </Grid>
+                </Container>
+            </Fragment>
+        );
+    }
 }
 export default Home;
